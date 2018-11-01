b2f8404c5346548f4269d4f80d79ca998c31df1e
jangsooha <tngk2582@nate.com>
2018-11-01 16:16:13 +0900

    second submission

--- main.c
+++ main.c
@@ -1,53 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ROWS 3
-#define	COLS 3
+
+#define input 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void printMatrix(int A[][COLS])
-{
-  int i, j;
-    
-  for(i=0; i<ROWS; i++){
-    for (j=0; j<COLS; j++){
-       printf("%d ", A[i][j]);  
-      }
-      printf("\n");
-   }
-    return;
-}
+//함수정의
+ void bubbleSort(){
 
 
-void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
-{
-    int i, j;
+  bubbleSort();
+}
 
-    for(i=0; i<ROWS; i++){
-      for (j=0; j<COLS; j++){
-       C[i][j]  = A[i][j] + B[i][j];
-      }
-}
-   return 1;
-}
 
 
 int main(int argc, char *argv[]) {
 
-    int A[ROWS][COLS]={
-     {2, 3, 0},
-     {8, 9, 1},
-     {7, 0, 5}  };
+    int A[input];
+    int i;
 
-    int B[ROWS][COLS]={
-     {1, 0, 0},
-     {0, 1, 0},
-     {0, 0, 1}  };
+   for(i=0; i<input; i++){
+       scanf("%d", &A[i]);  
+    }  
+    
 
-   int C[ROWS][COLS];
-
-   addMatrix(A, B, C);
-   printMatrix(C);
+   for(i=0; i<input; i++){
+       printf("\n\n%d", A[i]);  
+    }  
+     
+   
 
 	return 0;
 }
